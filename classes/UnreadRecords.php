c293a0e9ef2572fc631a084db5f263a4de51d358
Joseph Crowell <joseph.w.crowell@gmail.com>
2023-07-04 12:41:57 +1000

    Fix getting total count of records preventing install because the records table doesn't exist

--- classes/UnreadRecords.php
+++ classes/UnreadRecords.php
@@ -2,13 +2,17 @@
 
 namespace Martin\Forms\Classes;
 
+use Schema;
 use Martin\Forms\Models\Record;
 
 class UnreadRecords
 {
     public static function getTotal()
     {
-        $unread = Record::where('unread', 1)->count();
-        return ($unread > 0) ? $unread : null;
+        if (Schema::hasTable('martin_forms_records')) {
+            $unread = Record::where('unread', 1)->count();
+        }
+
+        return (isset($unread) && $unread > 0) ? $unread : null;
     }
 }
